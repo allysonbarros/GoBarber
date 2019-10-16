bc95091109725debf5167389c646a1925c3c64ef
Allyson Barros <allysonbarrosrn@gmail.com>
2019-10-15 21:54:33 -0300

    Subindo a aplicação até a aula 12 do módulo 3.

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,106 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+import NotificationSchema from '../schemas/Notification';
+
+class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const appointsments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        date: {
+          [Op.gte]: new Date(),
+        },
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointsments);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ message: 'Validation fails.' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ message: 'You can only create appointsments with providers.' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ message: 'Past dates are not permitted.' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ message: 'Appointment date is not available.' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "dd 'de' MMMM 'às' H:mm'.'", {
+      locale: pt,
+    });
+
+    await NotificationSchema.create({
+      content: `Novo agendamento de ${user.name} para o dia ${formattedDate}`,
+      user: provider_id,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '95a7bbd38b27a56ff76ae23dfb71c335',
+  expiresIn: '7d',
+};
